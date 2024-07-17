ae9f3b953833b7312bde750d357cb3647e49f363
Jassim Choudhary <166564157+jassimchoudharydev@users.noreply.github.com>
2024-07-17 20:21:38 +0100

    Categories section

--- src/data.js
+++ src/data.js
@@ -7,21 +7,39 @@ export const sliderItems = [
     id: 1,
     img: coupleImage1,
     title: "WINTER SALE",
-    desc: "UPGRADE YOUR LOOK WITH OUR WINTER SALE! ENJOY 30% OFF ON NEW ARRIVALS.",
+    desc: "STAY WARM AND STYLISH WITH OUR WINTER SALE! ENJOY 30% OFF ON NEW ARRIVALS.",
     bg: "d8e2dc",
   },
   {
     id: 2,
     img: coupleImage2,
     title: "NEW COLLECTION",
-    desc: "UPGRADE YOUR LOOK WITH OUR WINTER SALE! ENJOY 30% OFF ON NEW ARRIVALS.",
-    bg: "e8e8e4",
+    desc: "DISCOVER THE LATEST TRENDS WITH OUR NEW COLLECTION! UPGRADE YOUR WARDROBE TODAY.",
+    bg: "f8edeb",
   },
   {
     id: 3,
     img: coupleImage3,
     title: "LOUNGEWEAR LOVE",
-    desc: "UPGRADE YOUR LOOK WITH OUR WINTER SALE! ENJOY 30% OFF ON NEW ARRIVALS.",
+    desc: "RELAX IN STYLE WITH OUR LOUNGEWEAR LOVE COLLECTION! COMFORT AND FASHION COMBINED.",
     bg: "ece4db",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "URBAN ESSENTIALS",
+  },
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "STREETWEAR CLASSICS",
+  },
+  {
+    id: 3,
+    img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    title: "SEASONAL TRENDS",
   },
 ];
