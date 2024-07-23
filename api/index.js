4f01d7ce0899f5934f6ab1c3b3d7f50111055853
Jassim Choudhary <166564157+jassimchoudharydev@users.noreply.github.com>
2024-07-23 16:31:14 +0100

    Added express

--- api/index.js
+++ api/index.js
@@ -1,1 +1,8 @@
-console.log("hello");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const port = 5001;
+
+app.listen(port, () => {
+  console.log("Backend server is running.");
+});
