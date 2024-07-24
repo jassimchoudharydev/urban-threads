61287491c0eef9be5dea680325f7050f337fa51d
Jassim Choudhary <166564157+jassimchoudharydev@users.noreply.github.com>
2024-07-24 16:27:24 +0100

    Connected mongodb

--- api/index.js
+++ api/index.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const port = 5001;
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+const PORT = process.env.PORT || 5001;
+const dotenv = require("dotenv");
+dotenv.config();
 
-app.listen(port, () => {
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("DB connection successful."))
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(PORT, () => {
   console.log("Backend server is running.");
 });
